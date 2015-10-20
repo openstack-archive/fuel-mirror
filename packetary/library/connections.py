2c0eeae2493ce3a4a817fcfe619b3d4a000702e8
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-10-20 18:08:10 +0300

    [packetary] Infrastructure
    Introduce infrastructure:
    * Executor (The Asynchronous task executor)
    * ConnectionsPool (The pool of network connections)
    * ResumeableStream - allows resume streaming when error occures
    
    Change-Id: Ia68d60c2b9d685820a4c1916c8f1aa724f3a3f91
    Implements: blueprint refactor-local-mirror-scripts
    Partial-Bug: #1487077

---
+++ packetary/library/connections.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+import os
+import six
+import six.moves.urllib.request as urllib_request
+import six.moves.urllib_error as urllib_error
+import time
+
+from packetary.library.streams import BufferedStream
+
+
+logger = logging.getLogger(__package__)
+
+
+class RangeError(urllib_error.URLError):
+    pass
+
+
+class RetryableRequest(urllib_request.Request):
+    offset = 0
+    retries_left = 1
+    start_time = 0
+
+
+class ResumeableStream(BufferedStream):
+    def __init__(self, request, response, opener):
+        super(ResumeableStream, self).__init__(response)
+        self.request = request
+        self.opener = opener
+
+    def _read(self, chunksize):
+        while 1:
+            try:
+                chunk = self.fileobj.read(chunksize)
+                self.request.offset += len(chunk)
+                return chunk
+            except IOError as e:
+                response = self.opener.error(
+                    self.request.get_type(), self.request,
+                    self.fileobj, 502, six.text_type(e), self.fileobj.info()
+                )
+                self.fileobj = response.fileobj
+
+
+class RetryHandler(urllib_request.BaseHandler):
+    @staticmethod
+    def http_request(request):
+        logger.debug("start request: %s", request.get_full_url())
+        if request.offset > 0:
+            request.add_header('Range', 'bytes=%d-' % request.offset)
+        request.start_time = time.time()
+        return request
+
+    def http_response(self, request, response):
+        # the server should response partial content if range is specified
+        logger.debug(
+            "finish request: %s - %d (%s), duration - %d ms.",
+            request.get_full_url(), response.getcode(), response.msg,
+            int((time.time() - request.start_time) * 1000)
+        )
+        if request.offset > 0 and response.getcode() != 206:
+            raise RangeError("Server does not support ranges.")
+        return ResumeableStream(request, response, self.parent)
+
+    def http_error(self, req, fp, code, msg, hdrs):
+        if code >= 500 and req.retries_left > 0:
+            req.retries_left -= 1
+            logger.warning(
+                "fail request: %s - %d(%s), retries left - %d.",
+                req.get_full_url(), code, msg, req.retries_left
+            )
+            return self.parent.open(req)
+
+    https_request = http_request
+    https_response = http_response
+
+
+class Connection(object):
+    def __init__(self, opener, retries_num):
+        self.opener = opener
+        self.retries_num = retries_num
+
+    def get_request(self, url, offset=0):
+        if url.startswith("/"):
+            url = "file://" + url
+
+        request = RetryableRequest(url)
+        request.retries_left = self.retries_num
+        request.offset = offset
+        return request
+
+    def open_stream(self, url, offset=0):
+        """Opens remote file for streaming.
+
+        :param url: the remote file`s url
+        :param offset: the number of bytes from begin, that will be skipped
+        """
+
+        request = self.get_request(url, offset)
+        while 1:
+            try:
+                return self.opener.open(request)
+            except (RangeError, urllib_error.HTTPError):
+                raise
+            except IOError as e:
+                if request.retries_left <= 0:
+                    raise
+                request.retries_left -= 1
+                logger.exception(
+                    "Failed to open url: %s. retries left - %d.",
+                    six.text_type(e), request.retries_left
+                )
+
+    def retrieve(self, url, filename, offset=0):
+        """Downloads remote file.
+
+        :param url: the remote file`s url
+        :param filename: the file`s name, that includes path on local fs
+        :param offset: the number of bytes from begin, that will be skipped
+        """
+
+        self._ensure_dir_exists(filename)
+        fd = os.open(filename, os.O_CREAT | os.O_WRONLY)
+        try:
+            self._copy_stream(fd, url, offset)
+        except RangeError:
+            if offset == 0:
+                raise
+            logger.warning(
+                "Failed to resume download, starts from begin: %s", url
+            )
+            self._copy_stream(fd, url, 0)
+        finally:
+            os.fsync(fd)
+            os.close(fd)
+
+    @staticmethod
+    def _ensure_dir_exists(dst):
+        """Checks that directory exists and creates otherwise."""
+        target_dir = os.path.dirname(dst)
+        try:
+            os.makedirs(target_dir)
+        except OSError as e:
+            if e.errno != 17:
+                raise
+
+    def _copy_stream(self, fd, url, offset):
+        """Copies remote file to local.
+
+        :param fd: the file`s descriptor
+        :param url: the remote file`s url
+        :param offset: the number of bytes from begin, that will be skipped
+        """
+
+        source = self.open_stream(url, offset)
+        os.ftruncate(fd, offset)
+        os.lseek(fd, offset, os.SEEK_SET)
+        chunk_size = 16 * 1024
+        while 1:
+            chunk = source.read(chunk_size)
+            if not chunk:
+                break
+            os.write(fd, chunk)
+
+
+class ConnectionContext(object):
+    def __init__(self, connection, on_exit):
+        self.connection = connection
+        self.on_exit = on_exit
+
+    def __enter__(self):
+        return self.connection
+
+    def __exit__(self, *_):
+        self.on_exit(self.connection)
+
+
+class ConnectionsPool(object):
+    """Controls the number of simultaneously opened connections."""
+
+    MIN_CONNECTIONS_COUNT = 1
+
+    def __init__(self, options):
+        retries = options.get("retries_count", 0)
+        http_proxy = options.get("connection_proxy")
+        if http_proxy:
+            proxies = {
+                "http": http_proxy,
+            }
+        else:
+            proxies = None
+
+        opener = urllib_request.build_opener(
+            RetryHandler(),
+            urllib_request.ProxyHandler(proxies)
+        )
+
+        limit = max(
+            options.get("connection_count", 0), self.MIN_CONNECTIONS_COUNT
+        )
+        connections = six.moves.queue.Queue()
+        while limit > 0:
+            connections.put(Connection(opener, retries))
+            limit -= 1
+
+        self.free = connections
+
+    def get(self, timeout=None):
+        """Gets the free connection.
+
+        Blocks in case if there is no free connections.
+        """
+        return ConnectionContext(
+            self.free.get(timeout=timeout), self._release
+        )
+
+    def _release(self, connection):
+        """Puts back connection to free connections."""
+        self.free.put(connection)
---
+++ packetary/library/executor.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import with_statement
+
+import functools
+import logging
+import six
+import threading
+
+
+logger = logging.getLogger(__package__)
+
+
+def _callback(func):
+    """Wraps callback to ensure that it does not raise exception."""
+    @functools.wraps(func)
+    def wrapper(e):
+        try:
+            func(e)
+        except Exception as e:
+            logger.exception("Exception in callback: %s", six.text_type(e))
+    return wrapper
+
+
+class Executor(object):
+    """Asynchronous tasks executor."""
+
+    DEFAULT_THREADS_COUNT = 1
+    DEFAULT_QUEUE_SIZE = 100
+
+    _stopper = object()
+
+    def __init__(self, options):
+        threads_num = max(
+            options.get('threads_count', 0), self.DEFAULT_THREADS_COUNT
+        )
+        queue_size = options.get('queue_size') or self.DEFAULT_QUEUE_SIZE
+        self.tasks = six.moves.queue.Queue(maxsize=queue_size)
+        self.threads = threads = []
+        self._closed = False
+        while threads_num > 0:
+            t = threading.Thread(
+                target=Executor._worker,
+                args=(self,)
+            )
+            t.daemon = True
+            t.start()
+            threads.append(t)
+            threads_num -= 1
+
+    def execute(self, task, on_complete):
+        """Executes task asynchronous."""
+        if not self._closed:
+            self.tasks.put((task, _callback(on_complete)))
+
+    def shutdown(self, wait=True):
+        """Shutdowns executor."""
+        if self._closed:
+            return
+        logger.debug("Shutting down executor...")
+        threads_num = len(self.threads)
+        while threads_num:
+            self.tasks.put(self._stopper)
+            threads_num -= 1
+
+        if wait:
+            logger.debug("Waiting threads...")
+            while self.threads:
+                self.threads.pop().join()
+            logger.debug("Waiting jobs...")
+            self.tasks.join()
+        else:
+            while self.threads:
+                self.threads.pop()
+        self._closed = True
+        logger.debug("Completed.")
+
+    def _worker(self):
+        while True:
+            try:
+                task = self.tasks.get()
+                if task is Executor._stopper:
+                    break
+
+                func, on_complete = task
+                try:
+                    if self._closed:
+                        raise RuntimeError("Closed.")
+                    func()
+                    on_complete(None)
+                except Exception as e:
+                    on_complete(e)
+            finally:
+                self.tasks.task_done()
+
+
+class AsynchronousSection(object):
+    """Allows calling function asynchronously with waiting on exit."""
+
+    POLL_TIMEOUT = 5
+
+    def __init__(self, executor, ignore_errors_num):
+        """Initialises.
+
+        :param executor: the executor instance
+        :param ignore_errors_num:
+               number of errors which does not stop the execution
+        """
+        self.executor = executor
+        self.ignore_errors_num = ignore_errors_num
+        self.errors = 0
+        self.counter = 0
+        self.mutex = threading.Lock()
+        self.condition = threading.Condition(self.mutex)
+
+    def __enter__(self):
+        self.errors = 0
+        self.counter = 0
+        return self
+
+    def __exit__(self, etype, *_):
+        self.wait(etype is not None)
+
+    def execute(self, func, *args, **kwargs):
+        """Calls function asynchronously."""
+
+        if 0 <= self.ignore_errors_num < self.errors:
+            raise RuntimeError("Too many errors.")
+
+        self.executor.execute(
+            functools.partial(func, *args, **kwargs), self.on_complete
+        )
+        self.mutex.acquire()
+        try:
+            self.counter += 1
+        finally:
+            self.mutex.release()
+
+    def on_complete(self, err=None):
+        """Callback to catch task completion."""
+
+        if err is not None:
+            logger.exception("Task failed: %s", six.text_type(err))
+            delta = 1
+        else:
+            delta = 0
+
+        self.condition.acquire()
+        try:
+            self.errors += delta
+            self.counter -= 1
+            self.condition.notify_all()
+        finally:
+            self.condition.release()
+
+    def wait(self, ignore_errors=False):
+        """Waits until all tasks will be completed.
+
+        Do not use directly, will called from context manager.
+        """
+        self.condition.acquire()
+        try:
+            while self.counter > 0:
+                logger.debug("%d: tasks left - %d.", id(self), self.counter)
+                self.condition.wait(self.POLL_TIMEOUT)
+            logger.debug("%d: tasks left - 0.", id(self))
+        finally:
+            self.condition.release()
+
+        if not ignore_errors and self.errors > 0:
+            raise RuntimeError(
+                "Operations completed with errors. See log for more details."
+            )
