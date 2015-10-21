71f761d1c0b9fd8ac5c9153725bc34c48aaa163e
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-10-21 12:58:54 +0300

    [packetary] Repository class
    class Repository composes from:
    * RepositryDriver - low-level support for physical repository. deb, yum, etc.
    * RepositoryController - infrastcuture method to communicate with driver
    * RepositoryApi - high-level class, that provides methods to work with repository
    
    Change-Id: Iaf868fca982d91089e369d13a6fb381ff879ea73
    Implements: blueprint refactor-local-mirror-scripts
    Partial-Bug: #1487077
    Depends-On:  Ie2014aeee0c4776ed411ef6f28a996ef2d029c95

---
+++ packetary/tests/test_packages_tree.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import warnings
+
+from packetary.objects import Index
+from packetary.objects import PackagesTree
+from packetary.tests import base
+from packetary.tests.stubs import generator
+
+
+class TestPackagesTree(base.TestCase):
+    def setUp(self):
+        super(TestPackagesTree, self).setUp()
+
+    def test_get_unresolved_depends(self):
+        ptree = PackagesTree()
+        ptree.add(generator.gen_package(
+            1, requires=[generator.gen_relation("unresolved")]))
+        ptree.add(generator.gen_package(2, requires=None))
+        ptree.add(generator.gen_package(
+            3, requires=[generator.gen_relation("package1")]
+        ))
+        ptree.add(generator.gen_package(
+            4,
+            requires=[generator.gen_relation("loop")],
+            obsoletes=[generator.gen_relation("loop", ["le", 1])]
+        ))
+
+        unresolved = ptree.get_unresolved_depends()
+        self.assertItemsEqual(
+            ["loop", "unresolved"],
+            (x.name for x in unresolved)
+        )
+
+    def test_get_minimal_subset_with_master(self):
+        ptree = PackagesTree()
+        ptree.add(generator.gen_package(1, requires=None))
+        ptree.add(generator.gen_package(2, requires=None))
+        ptree.add(generator.gen_package(3, requires=None))
+        ptree.add(generator.gen_package(
+            4, requires=[generator.gen_relation("package1")]
+        ))
+
+        master = Index()
+        master.add(generator.gen_package(1, requires=None))
+        master.add(generator.gen_package(
+            5,
+            requires=[generator.gen_relation(
+                "package10",
+                alternative=generator.gen_relation("package4")
+            )]
+        ))
+
+        unresolved = set([generator.gen_relation("package3")])
+        resolved = ptree.get_minimal_subset(master, unresolved)
+        self.assertItemsEqual(
+            ["package3", "package4"],
+            (x.name for x in resolved)
+        )
+
+    def test_get_minimal_subset_without_master(self):
+        ptree = PackagesTree()
+        ptree.add(generator.gen_package(1, requires=None))
+        ptree.add(generator.gen_package(2, requires=None))
+        ptree.add(generator.gen_package(
+            3, requires=[generator.gen_relation("package1")]
+        ))
+        unresolved = set([generator.gen_relation("package3")])
+        resolved = ptree.get_minimal_subset(None, unresolved)
+        self.assertItemsEqual(
+            ["package3", "package1"],
+            (x.name for x in resolved)
+        )
+
+    def test_mandatory_packages_always_included(self):
+        ptree = PackagesTree()
+        ptree.add(generator.gen_package(1, requires=None, mandatory=True))
+        ptree.add(generator.gen_package(2, requires=None))
+        ptree.add(generator.gen_package(3, requires=None))
+        unresolved = set([generator.gen_relation("package3")])
+        resolved = ptree.get_minimal_subset(None, unresolved)
+        self.assertItemsEqual(
+            ["package3", "package1"],
+            (x.name for x in resolved)
+        )
+
+    def test_warning_if_unresolved(self):
+        ptree = PackagesTree()
+        ptree.add(generator.gen_package(
+            1, requires=None))
+
+        with warnings.catch_warnings(record=True) as log:
+            ptree.get_minimal_subset(
+                None, [generator.gen_relation("package2")]
+            )
+        self.assertIn("package2", str(log[0]))
