825089485f07fb7c50cb8c907105172a2ef6913d
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-12-02 14:09:49 +0300

    [8.0][build][deb] Implement supporting debian distros
       Related PROD: https://mirantis.jira.com/browse/PROD-2547
    
    Change-Id: I04049ad1c185bbd774a03a58ed98bfb87e5d3ea8

--- perestroika/docker-builder/create-deb-chroot.sh
+++ perestroika/docker-builder/create-deb-chroot.sh
@@ -5,7 +5,6 @@
 #
 # Usage:  DIST=trusty  ./create-deb-chroot.sh     # for Trusty
 #         DIST=precise ./create-deb-chroot.sh     # for Precise
-#         UPSTREAM_MIRROR=http://ua.archive.ubuntu.com/ubuntu/ ./create-deb-chroot.sh
 
 set -ex
 
@@ -15,21 +14,28 @@ source "${BIN}/config"
 
 CONTAINERNAME=sbuild:latest
 CACHEPATH=/var/cache/docker-builder/sbuild
-# define upstream Ubuntu mirror
-MIRROR=${UPSTREAM_MIRROR:-http://mirror.yandex.ru/ubuntu}
 # Use trusty distro by default
 [ -z "${DIST}" ] && DIST=trusty
 
-if [ "${DIST}" != "precise" ] && [ "${DIST}" != "trusty" ]; then
-  echo "Unknown dist version: ${DIST}"
-  exit 1
+if [ ! -f "${BIN}/sbuild/${DIST}-amd64-sbuild" ] ; then
+    echo "Unknown dist version: ${DIST}"
+    exit 1
 fi
 
+DIST_CONFIG="$(cat ${BIN}/sbuild/${DIST}-amd64-sbuild)"
+DIST_REPOS="$(cat ${BIN}/sbuild/${DIST}-amd64-aptsources)"
+MIRROR=$(echo "$DIST_REPOS" | head -1 | awk '{print $2}')
+
 docker run ${DNSPARAM} --privileged --rm -v ${CACHEPATH}:/srv/images ${CONTAINERNAME} \
-    bash -c "rm -f /etc/schroot/chroot.d/*
+    bash -c "rm -rf /etc/schroot/chroot.d
+             [ ! -d /srv/images/chroot.d ] && mkdir -p /srv/images/chroot.d
+             ln -s /srv/images/chroot.d /etc/schroot/chroot.d
+             rm -f /etc/schroot/chroot.d/${DIST}-amd64-sbuild*
+             rm -rf /srv/images/${DIST}-amd64
              sbuild-createchroot ${DIST} /srv/images/${DIST}-amd64 ${MIRROR}
-             echo deb ${MIRROR} ${DIST} main universe multiverse restricted > /srv/images/${DIST}-amd64/etc/apt/sources.list
-             echo deb ${MIRROR} ${DIST}-updates main universe multiverse restricted >> /srv/images/${DIST}-amd64/etc/apt/sources.list
+             mv /etc/schroot/chroot.d/${DIST}-amd64-sbuild* /etc/schroot/chroot.d/${DIST}-amd64-sbuild
+             echo 'union-type=aufs' >> /etc/schroot/chroot.d/${DIST}-amd64-sbuild
+             echo \"$DIST_REPOS\" > /srv/images/${DIST}-amd64/etc/apt/sources.list
              sbuild-update -udcar ${DIST}
              echo '#!/bin/bash' > /srv/images/${DIST}-amd64/usr/bin/apt-add-repo
              echo 'echo \$* >> /etc/apt/sources.list' >> /srv/images/${DIST}-amd64/usr/bin/apt-add-repo
