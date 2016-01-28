0dd20b30d447e8415f5c109641410a86b88b7509
Dzmitry Moisa <dmoisa@mirantis.com>
2016-01-28 10:48:02 +0300

    Check and monitoring tool for repo consistency
    In current workflow for every MOS project in review.fuel-infra.org we
    first merge the patch to the source repository and then trigger the
    job which publishes packages to the packages repository (deb- and rpm-
    repos).
    There are many circumstances which can cause publishing failures:
    network issues, hardware problems, software misconfiguration, etc. So
    this tool verify the content of the package repository is aligned with
    the code merged in Gerrit.
    
    Change-Id: I1ed0768b2e9261260c3b164e117cd057ae3dc817

---
+++ repo-monitor/check-package-functions.sh
@@ -0,0 +1,80 @@
+#===============================================================================================
+#                                          FUNCTIONS
+#===============================================================================================
+
+# Description:
+#    Get Gerrit project name by package name
+# Input:
+#     PACKAGE_NAME
+# Output:
+#     GERRIT_PROJECT
+get_gerrit_project () {
+    GERRIT_PROJECT=$(grep ${PACKAGE_NAME} ./projects.txt |awk '{print $2}')
+}
+
+# Description:
+#     Get Gerrit patch-set for the required package by timestamp
+# Input:
+#     GERRIT_PORT         << properties.sh
+#     GERRIT_USER         << properties.sh
+#     GERRIT_HOST         << properties.sh
+#     GERRIT_QUERY_FORMAT << properties.sh
+#     GERRIT_QUERY_LIMIT  << properties.sh
+#     GERRIT_COMMIT_INFO_FILE_PATH <<properties.sh
+#     PACKAGE_NAME
+#     TIME_BEFORE
+# Output:
+#     GERRIT_PROJECT_URL
+#     GERRIT_PROJECT_NAME
+#     GERRIT_REFSPEC
+#     GERRIT_MERGEDTO
+get_gerrit_properties () {
+    get_gerrit_project
+
+    ssh -p $GERRIT_PORT ${GERRIT_USER}@${GERRIT_HOST} gerrit query --format=$GERRIT_QUERY_FORMAT --current-patch-set project:{$GERRIT_PROJECT} before:{$TIME_BEFORE} status:merged limit:$GERRIT_QUERY_LIMIT > $GERRIT_COMMIT_INFO_FILE_PATH
+
+    GERRIT_PROJECT_URL="ssh://${GERRIT_USER}@${GERRIT_HOST}:${GERRIT_PORT}/${GERRIT_PROJECT}"
+    GERRIT_PROJECT_NAME=$(echo $GERRIT_PROJECT|awk -F '/' '{print $2}')
+    GERRIT_REFSPEC=$(grep 'ref:' $GERRIT_COMMIT_INFO_FILE_PATH | awk '{print $2}')
+    GERRIT_MERGEDTO=$(grep 'branch:' $GERRIT_COMMIT_INFO_FILE_PATH | awk '{print $2}')
+}
+
+# Description:
+#     Get sources from Gerrit of the required state
+# Input:
+#     GERRIT_PROJECT_URL
+#     PROJECT_SRC_DIR
+#     GERRIT_REFSPEC
+get_gerrit_patchset () {
+    git clone $GERRIT_PROJECT_URL $PROJECT_SRC_DIR
+    git -C $PROJECT_SRC_DIR fetch ${GERRIT_PROJECT_URL} ${GERRIT_REFSPEC} && git -C $PROJECT_SRC_DIR checkout FETCH_HEAD
+}
+
+# Description:
+#     Get package release from sources
+# Input:
+#     PROJECT_SRC_PSTH
+#     GERRIT_MERGEDTO
+# Output:
+#     GERRIT_MOS_RELEASE
+get_package_release () {
+    local release_tag=$(git -C ${PROJECT_SRC_DIR} describe --abbrev=0)
+    GERRIT_MOS_RELEASE=$(git -C ${PROJECT_SRC_DIR} rev-list --no-merges ${release_tag}..origin/${GERRIT_MERGEDTO} | wc -l)
+}
+
+get_deb_package_release () {
+    get_package_release
+}
+
+get_fuel_deb_package_version () {
+  version=$(git -C ${srcpath} rev-list --no-merges origin/${SOURCE_BRANCH} | wc -l)
+}
+
+get_rpm_package_release () {
+    get_package_release
+}
+
+#TBD
+get_fuel_rpm_package_version () {
+   echo "NOT IMPLEMENTED"
+}
---
+++ repo-monitor/properties.sh
@@ -0,0 +1,14 @@
+TEMP_DIR="./tmp"
+SRC_DIR="./src"
+
+PACKAGES_REPO_HOST="perestroika-repo-tst.infra.mirantis.net/mos-repos"
+
+GERRIT_HOST="review.fuel-infra.org"
+GERRIT_PORT="29418"
+GERRIT_USER="dmoisa"
+
+GERRIT_QUERY_FORMAT="TEXT"
+GERRIT_QUERY_LIMIT="1"
+GERRIT_COMMIT_INFO_FILE_PATH="${TEMP_DIR}/commit_info.txt"
+
+PROJECT_INFO_FILE="./projects.txt"
