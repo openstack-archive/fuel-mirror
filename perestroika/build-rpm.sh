a79627345afe38c5cfe1aec35ba710e24aa23d0e
Dmitry Burmistrov <dburmistrov@mirantis.com>
2016-03-09 19:38:06 +0300

    [build] Do not include CR repositories at build stage
    Partial-Bug: #1575171
    
    Change-Id: I4beb448d5b3073033cfa1df6580680cb9153fc66
    Partial-Bug: #1511757
    (cherry picked from commit 4af7fc2e9cccbb99ffd3651462bf2c4f1a5ef9c5)

--- perestroika/build-rpm.sh
+++ perestroika/build-rpm.sh
@@ -120,9 +120,9 @@ This package provides the %{-n*} kernel modules
 
   [ "$IS_UPDATES" == 'true' ] && \
     EXTRAREPO="${EXTRAREPO}|repo2,http://${REMOTE_REPO_HOST}/${RPM_PROPOSED_REPO_PATH}/x86_64"
-  [ "$GERRIT_CHANGE_STATUS" == "NEW" ] && [ "$IS_UPDATES" != "true" ] && \
+  [ "$GERRIT_CHANGE_STATUS" == "NEW" ] && [ "$IS_UPDATES" != "true" ] && [ -n "$LP_BUG" ] && \
     EXTRAREPO="${EXTRAREPO}|repo3,http://${REMOTE_REPO_HOST}/${REPO_REQUEST_PATH_PREFIX}/${REQUEST}/${RPM_OS_REPO_PATH}/x86_64"
-  [ "$GERRIT_STATUS" == "NEW" ] && [ "$IS_UPDATES" == "true" ] && \
+  [ "$GERRIT_STATUS" == "NEW" ] && [ "$IS_UPDATES" == "true" ] && [ -n "$LP_BUG" ] && \
     EXTRAREPO="${EXTRAREPO}|repo3,http://${REMOTE_REPO_HOST}/${REPO_REQUEST_PATH_PREFIX}/${REQUEST}/${RPM_PROPOSED_REPO_PATH}/x86_64"
   export EXTRAREPO
 
