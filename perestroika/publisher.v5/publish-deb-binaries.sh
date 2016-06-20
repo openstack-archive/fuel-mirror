971012218e95c1c7dc460647613aea89c3175eed
Sergey Otpuschennikov <sotpuschennikov@mirantis.com>
2016-06-20 13:40:35 +0300

    Implement Sigul for signing packages
     Signing rpm packages
     Signing deb packages
     Signing metadata of repository
    
    Related Jira-ticket: https://mirantis.jira.com/browse/PROD-3854
    
    Change-Id: I31cfe16bab67c003fa8687ac33a7fc6cc420cdf6

--- perestroika/publisher.v5/publish-deb-binaries.sh
+++ perestroika/publisher.v5/publish-deb-binaries.sh
@@ -7,6 +7,7 @@ source $(dirname $(readlink -e $0))/functions/locking.sh
 main() {
   local SIGN_STRING=""
   check-gpg && SIGN_STRING="true"
+  check-sigul ${SIGKEYID} ${SIGUL_USER} && USE_SIGUL="true"
 
   ## Download sources from worker
   [ -d $TMP_DIR ] && rm -rf $TMP_DIR
@@ -76,9 +77,13 @@ main() {
                   -i ${release_file}
               rm -f ${release_file}.gpg
               # ReSign Release file
-              [ -n "${SIGN_STRING}" ] \
+              if [ "${USE_SIGUL}" = "true" ] ; then
+                  sigul -u ${SIGUL_USER} sign-data -o "${release_file}.gpg" ${SIGKEYID} ${release_file}
+              else
+                  [ -n "${SIGN_STRING}" ] \
                   && gpg --sign --local-user ${SIGKEYID} -ba \
                   -o ${release_file}.gpg ${release_file}
+              fi
           done
           job_lock ${CONFIGDIR}.lock unset
       fi
@@ -183,9 +188,15 @@ main() {
   rm -f ${release_file}.gpg
   local pub_key_file="${LOCAL_REPO_PATH}/public/archive-${PROJECT_NAME}${PROJECT_VERSION}.key"
   if [ -n "${SIGN_STRING}" ] ; then
-      gpg --sign --local-user ${SIGKEYID} -ba -o ${release_file}.gpg ${release_file}
-      [ ! -f "${pub_key_file}" ] && touch ${pub_key_file}
-      gpg -o ${pub_key_file}.tmp --armor --export ${SIGKEYID}
+      if [ "${USE_SIGUL}" = "true" ] ; then
+          sigul -u ${SIGUL_USER} sign-data -o "${release_file}.gpg" ${SIGKEYID} ${release_file}
+          [ ! -f "${pub_key_file}" ] && touch ${pub_key_file}
+          sigul -u "${SIGUL_USER}" get-public-key ${SIGKEYID} > "${pub_key_file}.tmp"
+      else
+          gpg --sign --local-user ${SIGKEYID} -ba -o ${release_file}.gpg ${release_file}
+          [ ! -f "${pub_key_file}" ] && touch ${pub_key_file}
+          gpg -o ${pub_key_file}.tmp --armor --export ${SIGKEYID}
+      fi
       if diff -q ${pub_key_file} ${pub_key_file}.tmp &>/dev/null ; then
           rm ${pub_key_file}.tmp
       else
