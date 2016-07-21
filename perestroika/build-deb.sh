656325464fadb55e3ff420ff8b566976d784c1bf
Dmitry Burmistrov <dburmistrov@mirantis.com>
2016-07-21 15:40:32 +0300

    [build] Implement security flow
        Spec: https://review.fuel-infra.org/19129
    
    Change-Id: I4de35fc611c853e14db7705284936ed0c7a59a5d

--- perestroika/build-deb.sh
+++ perestroika/build-deb.sh
@@ -50,6 +50,8 @@ main () {
               && _rev=$(( $_rev + 1 ))
           [ "$IS_HOTFIX" == "true" ] \
               && _rev=$(get_extra_revision hotfix ${_srcpath} ${release_tag})
+          [ "$IS_SECURITY" == "true" ] \
+              && _rev=$(get_extra_revision security ${_srcpath} ${release_tag})
           if [ "$IS_PLUGIN" = "true" ]
           then
               version=${version}.dev${_rev}
@@ -132,24 +134,39 @@ main () {
   DEB_HOTFIX_DIST_NAME=${DEB_HOTFIX_DIST_NAME:-hotfix}
   [ -n "${EXTRAREPO}" ] && EXTRAREPO="${EXTRAREPO}|"
   EXTRAREPO="${EXTRAREPO}http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_DIST_NAME} ${COMPONENTS}"
-  if [ "$IS_HOTFIX" == "true" ] ; then
-      EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_HOTFIX_DIST_NAME} ${COMPONENTS}"
-      EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_UPDATES_DIST_NAME} ${COMPONENTS}"
-  else
-      [ "$IS_UPDATES" == 'true' ] \
-          && EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_PROPOSED_DIST_NAME} ${COMPONENTS}"
-  fi
+  case true in
+      "$IS_HOTFIX" )
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_HOTFIX_DIST_NAME} ${COMPONENTS}"
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_UPDATES_DIST_NAME} ${COMPONENTS}"
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_SECURITY_DIST_NAME} ${COMPONENTS}"
+          ;;
+      "$IS_SECURITY" )
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_UPDATES_DIST_NAME} ${COMPONENTS}"
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_SECURITY_DIST_NAME} ${COMPONENTS}"
+          ;;
+      "$IS_UPDATES" )
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_PROPOSED_DIST_NAME} ${COMPONENTS}"
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_UPDATES_DIST_NAME} ${COMPONENTS}"
+          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${DEB_REPO_PATH} ${DEB_SECURITY_DIST_NAME} ${COMPONENTS}"
+          ;;
+  esac
 
   if [ "$GERRIT_CHANGE_STATUS" == "NEW" ] && [ -n "$LP_BUG" -o -n "$CUSTOM_REPO_ID" ] ; then
-      if [ "$IS_UPDATES" == "true" ] ; then
-          if [ "$IS_HOTFIX" == "true" ] ; then
-              EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${REPO_REQUEST_PATH_PREFIX}/${REQUEST}/${DEB_REPO_PATH} ${DEB_HOTFIX_DIST_NAME} ${COMPONENTS}"
-          else
-              EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${REPO_REQUEST_PATH_PREFIX}/${REQUEST}/${DEB_REPO_PATH} ${DEB_PROPOSED_DIST_NAME} ${COMPONENTS}"
-          fi
-      else
-          EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${REPO_REQUEST_PATH_PREFIX}/${REQUEST}/${DEB_REPO_PATH} ${DEB_DIST_NAME} ${COMPONENTS}"
-      fi
+      case true in
+          "$IS_HOTFIX" )
+              local DEB_REQUEST_DIST_NAME=$DEB_HOTFIX_DIST_NAME
+              ;;
+          "$IS_SECURITY" )
+              local DEB_REQUEST_DIST_NAME=$DEB_SECURITY_DIST_NAME
+              ;;
+          "$IS_UPDATES" )
+              local DEB_REQUEST_DIST_NAME=$DEB_PROPOSED_DIST_NAME
+              ;;
+          *)
+              local DEB_REQUEST_DIST_NAME=$DEB_DIST_NAME
+              ;;
+      esac
+      EXTRAREPO="${EXTRAREPO}|http://${REMOTE_REPO_HOST}/${REPO_REQUEST_PATH_PREFIX}/${REQUEST}/${DEB_REPO_PATH} ${DEB_REQUEST_DIST_NAME} ${COMPONENTS}"
   fi
 
   export EXTRAREPO
