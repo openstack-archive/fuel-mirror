84307dbfb5706c007b04c94c4c2950f991db2c23
Dmitry Burmistrov <dburmistrov@mirantis.com>
2017-01-10 17:31:54 +0400

    [build] Customise fuel pkg revision
       Allow to change fuel package revision from CI side
    
       Related-Bug: #1651771
    
    Change-Id: I10e158256e8c0f7cd32f74ba05bfdb1cf376a83a

--- perestroika/build-fuel-deb.sh
+++ perestroika/build-fuel-deb.sh
@@ -46,10 +46,14 @@ main () {
         [ "$GERRIT_CHANGE_STATUS" == "NEW" ] && _rev=$(( $_rev + 1 ))
         [ "$IS_HOTFIX" == "true" ] \
             && _rev=$(get_extra_revision hotfix ${_srcpath})
-        local release="1~u14.04+mos${_rev}"
         # if gitshasrc is not defined (we are not using fetch_upstream), let's do it
         [ -n "${gitshasrc}" ] || local gitshasrc=$(git -C $_srcpath log -1 --pretty="%h")
-        [ "$GERRIT_CHANGE_STATUS" == "NEW" ] && release="${release}+git.${gitshasrc}"
+        if [ "$GERRIT_CHANGE_STATUS" == "NEW" ] ; then
+            local OVERRIDE_PKG_REVISION=${OVERRIDE_PKG_REVISION:-1}
+            local release="${OVERRIDE_PKG_REVISION}~u14.04+mos${_rev}+git.${gitshasrc}"
+        else
+            local release="1~u14.04+mos${_rev}"
+        fi
         local fullver=${version}-${release}
         # Update version and changelog
         local firstline=1
